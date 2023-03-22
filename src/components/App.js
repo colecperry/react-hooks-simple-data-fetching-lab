4634debb27ef5f4ff216305673ff44f6288f61f6
Cole Perry <colecperry@gmail.com>
2023-03-22 15:21:23 -0700

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import {useState, useEffect} from "react";
+
+function App() {
+    const [randomDog, setRandomDog] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then ((data) => {
+            setRandomDog(data.message);
+            setIsLoaded(true);
+            console.log(data);
+        });
+    }, []);
+    if (!isLoaded) return <p>Loading...</p>
+
+    return <img src={randomDog} alt="A Random Dog"></img>
+}
+
+
+export default App;
